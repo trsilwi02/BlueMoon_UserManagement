a586db6a7659b2fb23861cea1f3c63c4340387cf
donllibee <nguyenhoangduong2202@gmail.com>
2025-11-25 16:53:27 +0700

    temp

---
+++ FrontEnd/main_layout/sidebar.js
@@ -0,0 +1,22 @@
+async function loadSidebar() {
+  try {
+    // 1. Gửi request đọc file sidebar.html
+    // const response = await fetch("../main_layout/sidebar.html"); //await giúp dừng hàm tại đây cho đến khi fetch xong
+    const response = await fetch("/FrontEnd/main_layout/sidebar.html"); //await giúp dừng hàm tại đây cho đến khi fetch xong
+
+    // 2. Kiểm tra nếu file không tồn tại
+    if (!response.ok) {
+      throw new Error("Không tìm thấy sidebar.html");
+    }
+
+    // 3. Lấy nội dung file dưới dạng text
+    const html = await response.text();
+
+    // 4. Chèn nội dung vào thẻ <div id="sidebar">
+    document.getElementById("sidebar").innerHTML = html;
+  } catch (error) {
+    console.error("Lỗi load sidebar:", error);
+  }
+}
+
+loadSidebar(); // Gọi hàm
