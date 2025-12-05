9005a9b27a51c47b43340499d9608040d3864c35
bao41 <bao.ld235016@sis.hust.edu.vn>
2025-12-05 16:23:05 +0700

    User

--- BackEnd/routes/UserRoute.js
+++ BackEnd/routes/UserRoute.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const LoginController = require('../controllers/LoginController.js');
 
+const UserController = require('../controllers/UserController');
+const LoginController = require('../controllers/LoginController');
+
+// API đăng ký
+router.post('/register', UserController.register);
+
+// API đăng nhập
 router.post('/login', LoginController.login);
 
-module.exports = router;
+module.exports = router;   
